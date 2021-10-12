c4114924113907fee4d307c9381c2a4db7c8b22a
May <MayyFaucher@gmail.com>
2021-10-11 18:25:17 -0700

    refractored all the code

--- script.js
+++ script.js
@@ -12,8 +12,8 @@ function email() {
 }
 function phone() {
     this.innerHTML = "(707) 672-4018"
-    
-    callLink.setAttribute("href","tel:7076724018");
+
+    callLink.setAttribute("href", "tel:7076724018");
 }
 
 eButt.addEventListener("click", email);
